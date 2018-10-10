959cfd3597bc5e132c4109731487d5510bc9768f
HighClassGrey <tamekaahannah@gmail.com>
2018-10-10 17:34:26 -0500

    finished

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-let row = document.querySelectorAll('.row');
-let currentPlayer = "X";
+let row = document.querySelectorAll('.row div');
+console.log(row)
+let currentPlayer = 'X';
+
+
 
 
 row.forEach(function (row) {
@@ -7,13 +10,10 @@ row.forEach(function (row) {
 });
 
 function cellClicked(e) {
-    // if () {
-
-    // }
     e.target.textContent = currentPlayer;
     togglePlayer();
 
-
+    checkRow();
 
 
 }
@@ -24,5 +24,51 @@ function togglePlayer() {
     } else {
         currentPlayer = 'X';
     };
-    
+
 };
+
+function checkRow() {
+    if (row[0].textContent === row[1].textContent && row[0].textContent === row[2].textContent) {
+        alert("You are the Winner"); 
+    } else if (row[3].textContent === row[4].textContent && row[3].textContent === row[5].textContent) {
+        alert("You are the Winner");
+    } else if (row[6].textContent === row[7].textContent && row[6].textContent === row[8].textContent) {
+        alert("You are the Winner");
+    } else {
+        checkCol();
+    }
+};
+
+function checkCol() {
+    if (row[0].textContent === row[3].textContent && row[0].textContent === row[6].textContent) {
+        alert("You are the Winner");
+    } else if (row[1].textContent === row[4].textContent && row[1].textContent === row[7].textContent) {
+        alert("You are the Winner"); 
+    } else if (row[2].textContent === row[5].textContent && row[2].textContent === row[8].textContent) {
+        alert("You are the Winner");
+    } else {
+        checkAng();
+    }
+};
+
+function checkAng() {
+    if (row[0].textContent === row[4].textContent && row[0].textContent === row[8].textContent) {
+        alert("You are the Winner");
+    } else if (row[2].textContent === row[4].textContent && row[2].textContent === row[6].textContent) {
+        alert("You are the Winner");
+    } else {
+        console.log('no winners)');
+    }
+};
+
+
+// function checkMid() {
+//     if (row[3].textContent === row[4].textContent  && row[0].textContent  === row[2].textContent) {
+//         console.log("we have a winner");
+//     };
+
+// };
+    // } else if (row[3].textContent == row[4].textContent  && row[3].textContent == row[5].textContent) {
+    //     console.log("row 2 a winner");
+    // } else  (row[6].textContent == row[7].textContent && row[6].textContent == row[8].textContent) 
+    //     console.log ('row 3 is the winner')
