f4f222f1744ba14c2ec3e5d19c1bb62539830d42
HighClassGrey <tamekaahannah@gmail.com>
2018-10-11 23:30:41 -0500

    Finished

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 let row = document.querySelectorAll('.row div');
-console.log(row)
 let currentPlayer = 'X';
-
-
-
+let gameOver = false;
+let reset = document.querySelectorAll('button')
 
 row.forEach(function (row) {
     row.addEventListener("click", cellClicked);
 });
 
+
+
 function cellClicked(e) {
+
+    if (gameOver === true) {
+        resetBoard();
+    }
+    if (e.target.textContent === 'X' || e.target.textContent === 'O') {
+        return;
+    };
+
     e.target.textContent = currentPlayer;
+
     togglePlayer();
-
     checkRow();
-
-
 }
+
+
 
 function togglePlayer() {
     if (currentPlayer === 'X') {
@@ -24,51 +32,54 @@ function togglePlayer() {
     } else {
         currentPlayer = 'X';
     };
-
 };
 
+
 function checkRow() {
-    if (row[0].textContent === row[1].textContent && row[0].textContent === row[2].textContent) {
-        alert("You are the Winner"); 
-    } else if (row[3].textContent === row[4].textContent && row[3].textContent === row[5].textContent) {
-        alert("You are the Winner");
-    } else if (row[6].textContent === row[7].textContent && row[6].textContent === row[8].textContent) {
-        alert("You are the Winner");
+    if (row[0].textContent > " " && row[0].textContent === row[1].textContent && row[0].textContent === row[2].textContent) {
+        alert(row[0].textContent + " you are the winner!");
+        gameOver=true;
+    } else if (row[3].textContent > " " && row[3].textContent === row[4].textContent && row[3].textContent === row[5].textContent) {
+        alert(row[3].textContent + " you are the winner!");
+        gameOver=true;
+    } else if (row[6].textContent > " " && row[6].textContent === row[7].textContent && row[6].textContent === row[8].textContent) {
+        alert(row[6].textContent + " you are the winner!");
+        gameOver=true;
     } else {
         checkCol();
     }
 };
 
 function checkCol() {
-    if (row[0].textContent === row[3].textContent && row[0].textContent === row[6].textContent) {
-        alert("You are the Winner");
-    } else if (row[1].textContent === row[4].textContent && row[1].textContent === row[7].textContent) {
-        alert("You are the Winner"); 
-    } else if (row[2].textContent === row[5].textContent && row[2].textContent === row[8].textContent) {
-        alert("You are the Winner");
+    if (row[0].textContent > " " && row[0].textContent === row[3].textContent && row[0].textContent === row[6].textContent) {
+        alert(row[0].textContent + " you are the winner!");
+        gameOver=true;
+    } else if (row[1].textContent != " " && row[1].textContent === row[4].textContent && row[1].textContent === row[7].textContent) {
+        alert(row[1].textContent + " you are the winner!");
+        gameOver=true;
+    } else if (row[2].textContent > " " && row[2].textContent === row[5].textContent && row[2].textContent === row[8].textContent) {
+        alert(row[2].textContent + " you are the winner!");
+        gameOver=true;
     } else {
         checkAng();
     }
 };
 
 function checkAng() {
-    if (row[0].textContent === row[4].textContent && row[0].textContent === row[8].textContent) {
-        alert("You are the Winner");
-    } else if (row[2].textContent === row[4].textContent && row[2].textContent === row[6].textContent) {
-        alert("You are the Winner");
+    if (row[0].textContent > " " && row[0].textContent === row[4].textContent && row[0].textContent === row[8].textContent) {
+        alert(row[0].textContent + " you are the winner!");
+        gameOver=true;
+    } else if (row[2].textContent > " " && row[2].textContent === row[4].textContent && row[2].textContent === row[6].textContent) {
+        alert(row[2].textContent + " you are the winner!");
+        gameOver=true;
     } else {
         console.log('no winners)');
     }
 };
+function resetBoard() {
+    row.forEach(function (row) {
+        row.textContent = " ";
+    });
 
-
-// function checkMid() {
-//     if (row[3].textContent === row[4].textContent  && row[0].textContent  === row[2].textContent) {
-//         console.log("we have a winner");
-//     };
-
-// };
-    // } else if (row[3].textContent == row[4].textContent  && row[3].textContent == row[5].textContent) {
-    //     console.log("row 2 a winner");
-    // } else  (row[6].textContent == row[7].textContent && row[6].textContent == row[8].textContent) 
-    //     console.log ('row 3 is the winner')
+    gameOver = false;
+};
